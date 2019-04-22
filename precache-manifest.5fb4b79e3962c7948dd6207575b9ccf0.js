e66aeb14b72eb9575c2c5648833e0b4f3389844e
moonfalljade <moonfalljade@hotmail.com>
2019-04-23 06:03:39 +0800

    Updates

---
+++ precache-manifest.5fb4b79e3962c7948dd6207575b9ccf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c0bbf8aecd297c26580",
+    "url": "/would-you-rather/static/js/runtime~main.2bbe9c67.js"
+  },
+  {
+    "revision": "a06936360af392828c27",
+    "url": "/would-you-rather/static/js/main.02b744cf.chunk.js"
+  },
+  {
+    "revision": "6a681b775082a37b8bae",
+    "url": "/would-you-rather/static/js/2.72e984d5.chunk.js"
+  },
+  {
+    "revision": "a06936360af392828c27",
+    "url": "/would-you-rather/static/css/main.9d0abdfe.chunk.css"
+  },
+  {
+    "revision": "6a681b775082a37b8bae",
+    "url": "/would-you-rather/static/css/2.c70ab6f5.chunk.css"
+  },
+  {
+    "revision": "0642ac8d9f623a70c4969212e40229e6",
+    "url": "/would-you-rather/index.html"
+  }
+];
